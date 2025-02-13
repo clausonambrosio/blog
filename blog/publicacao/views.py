6761536fb55b788d22ab0ebcfedf9bcd21f3a33a
clausonambrosio <clausonambrosio@hotmail.com>
2025-02-13 19:16:15 -0300

    primeiro

--- blog/publicacao/views.py
+++ blog/publicacao/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Postagem  
 
 # Create your views here.
 
 def lista_postagens(request):  
     postagens = Postagem.objects.all()  
-    return render(request, 'postagens/lista.html', {'postagens': postagens})
+    return render(request, 'lista.html', {'postagens': postagens})
